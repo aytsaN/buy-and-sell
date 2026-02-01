38e0c17e28382dfa3bdd2868d0c9be9b2b217a44
Anastasiya_Shamal <anastasiya_shamal@epam.com>
2026-02-01 21:07:15 +0100

    feat: add BE and reorginize folder structure

---
+++ buy-and-sell-backend/src/routes/getListing.js
@@ -0,0 +1,13 @@
+import { Boom, notFound } from "@hapi/boom";
+import { fakeListings } from "./fake-data";
+
+export const getListingsRoute = {
+  method: 'GET',
+  path: '/api/listings/{id}',
+  handler: (req, h) => {
+    const id = req.params.id;
+    const listing = fakeListings.find(listing => listing.id === id);
+    if (!listing) throw notFound(`Listing with id ${id} not found`);
+    return listing;
+  }
+}
